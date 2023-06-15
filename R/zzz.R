9176545cc6aaa18a7bb04c69d28492be22be7d26
Lisa Breckels <lms79@cam.ac.uk>
2023-06-15 16:33:47 +0100

    rename pRoloc.novelty

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,10 @@
 .onAttach <- function(libname, pkgname) {
     packageStartupMessage(
-      paste("\nThis is pRoloc version", packageVersion("pRoloc"), "\n",
-            " Visit https://lgatto.github.io/pRoloc/ to get started.\n"))
+      paste("\nThis is pRoloc version", packageVersion("pRoloc.novelty"), "\n",
+            " a temporary version of pRoloc for using noveltyTAGM"))
 
     if (interactive() && .Platform$OS.type == "windows" &&
         .Platform$GUI == "Rgui") {
-        Biobase::addVigs2WinMenu("pRoloc")
+        Biobase::addVigs2WinMenu("pRoloc.novelty")
     }
 }
